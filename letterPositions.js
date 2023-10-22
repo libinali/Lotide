fe7246b7c666d4fdcdf595f50953434936747959
libinali <ali.libin@yahoo.com>
2023-10-22 21:23:43 +0000

    Added AI suggestions to test function

--- letterPositions.js
+++ letterPositions.js
@@ -39,4 +39,8 @@ const letterPositions = function(sentence) {
   return results;
 };
 
-assertArraysEqual(letterPositions("hello").o, [4]);
+const result = letterPositions("hello");
+assertArraysEqual(result.h, [0]);
+assertArraysEqual(result.e, [1]);
+assertArraysEqual(result.l, [2, 3]);
+assertArraysEqual(result.o, [4]);
