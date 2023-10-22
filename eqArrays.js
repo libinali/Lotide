7aa840621a6cd5cb3f4e15a4fa5459307373e5b9
libinali <ali.libin@yahoo.com>
2023-10-22 03:55:47 +0000

    Fixed error and added a middle function

--- eqArrays.js
+++ eqArrays.js
@@ -15,11 +15,11 @@ const eqArrays = function(arr1, arr2) {
     if (arr1[i] !== arr2[i]) {
       return false;
     }
-    return true;
   }
+  return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4, 5]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 
 eqArrays([1, 2, 3], [1, 2, 3]); // => true
 eqArrays([1, 2, 3], [3, 2, 1]); // => false
--- middle.js
+++ middle.js
@@ -24,4 +24,25 @@ const assertArraysEqual = function(actual, expected) {
 
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3], true);
+const middle = function(arr) {
+  const num1 = arr[Math.floor(arr.length / 2)];
+  const num2 = arr[Math.floor(arr.length / 2) - 1];
+
+  if (arr.length === 1 || arr.length === 2) {
+    return [];
+  }
+  
+  if (arr.length % 2 === 1) {
+    return [num1];
+  }
+
+  if (arr.length % 2 === 0) {
+    return [num2, num1];
+  }
+
+};
+
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
