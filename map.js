6dc89d2b7ecb18390f1f47133d5a6abf34c93794
libinali <ali.libin@yahoo.com>
2023-10-24 01:13:37 +0000

    First Commit

---
+++ map.js
@@ -0,0 +1,45 @@
+const assertArraysEqual = function(actual, expected) {
+  const areEqual = eqArrays(actual, expected);
+
+  if (areEqual) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  }
+
+  if (!areEqual) {
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+const greetings = ["hello", "ciao", "salam"];
+const fruits = ["apple", "grapes", "watermelon", "pear", "orange"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+
+assertArraysEqual(map(words, word => word[0]), [ 'g', 'c', 't', 'm', 't' ]);
+assertArraysEqual(map(greetings, word => word[0]), [ 'h', 'c', 's']);
+assertArraysEqual(map(fruits, word => word[0]), [ 'a', 'g', 'w', 'p', 'o']);
+
+
+
