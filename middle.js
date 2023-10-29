dbd04630bdf2eff50d5d3f5aea735b752d3e9053
libinali <ali.libin@yahoo.com>
2023-10-29 23:20:10 +0000

    Refactor middle

--- middle.js
+++ middle.js
@@ -1,29 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  const areEqual = eqArrays(actual, expected);
-
-  if (areEqual) {
-    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
-  }
-
-  if (!areEqual) {
-    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
-  }
-
-};
-
 const middle = function(arr) {
   const num1 = arr[Math.floor(arr.length / 2)];
   const num2 = arr[Math.floor(arr.length / 2) - 1];
@@ -42,7 +16,5 @@ const middle = function(arr) {
 
 };
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+module.exports = middle;
+
