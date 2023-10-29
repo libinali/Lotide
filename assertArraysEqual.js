3dbfa5605c296f41e7ab3efc9862f6f14681434a
libinali <ali.libin@yahoo.com>
2023-10-29 23:01:44 +0000

    Refactor assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays')
 
 const assertArraysEqual = function(actual, expected) {
   const areEqual = eqArrays(actual, expected);
@@ -24,4 +13,5 @@ const assertArraysEqual = function(actual, expected) {
 
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3], true);
+module.exports = assertArraysEqual;
+
