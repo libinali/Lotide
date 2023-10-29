70efac8c791364b6f8d26486462dd898780408d6
libinali <ali.libin@yahoo.com>
2023-10-29 22:42:55 +0000

    Refactor tail

--- tail.js
+++ tail.js
@@ -1,15 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(arr) {
   const noHead = arr.slice(1);
   return noHead;
 };
 
-assertEqual(tail([5,6,7]), [6,7]);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+module.exports = tail;
