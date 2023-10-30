6120cc26c9c0a6446ea84c00b8939aa8e36f3bb6
libinali <ali.libin@yahoo.com>
2023-10-30 00:35:48 +0000

    Refactor with Mocha and Chai I

--- test/headTest.js
+++ test/headTest.js
@@ -5,8 +5,8 @@ describe("#head", () => {
   it("returns 1 for [1, 2, 3]", () => {
     assert.strictEqual(head([1, 2, 3]), 1);
   });
-  it("returns '2' for ['2']", () => {
-    assert.strictEqual(head(['2']), '2'); 
+  it("returns '5' for ['5', '6', '7']", () => {
+    assert.strictEqual(head(['5', '6', '7']), '5'); 
   });
   it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
     assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,14 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// TEST CODE
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); 
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); 
-assertEqual(words.length, 3);
+describe("#tail", () => {
+  it("returns [6, 7] for [5, 6, 7]", () => {
+    assert.deepEqual(tail([5, 6, 7]), [6, 7]);
+  });
+  it('returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it("returns [2, 3, 4] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4]), [2, 3, 4]);
+  });
+});
